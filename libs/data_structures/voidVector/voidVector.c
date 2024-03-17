729c59e637847a35c39ca65da6d5d59ee99792f4
Sofia Chupahina PV-231 <sovachupahina@yandex.ru>
2024-03-17 23:03:02 +0300

    memory usage of vectorVoid

--- libs/data_structures/voidVector/voidVector.c
+++ libs/data_structures/voidVector/voidVector.c
@@ -1,1 +1,40 @@
 #include "voidVector.h"
+#include "C:\Users\Анна\Desktop\сонины программы\second_semester\WorksOfSecondSemester\libs\algorithms\array\array.c"
+#include "C:\Users\Анна\Desktop\сонины программы\second_semester\WorksOfSecondSemester\libs\algorithms\math_basics\math_basics.c"
+#include <malloc.h>
+#include <stdio.h>
+
+vectorVoid createVector(size_t n, size_t baseTypeSize) {
+    int *data = malloc(baseTypeSize*n);
+    if (data == NULL && n != 0) {
+        fprintf(stderr, "bad alloc");
+        exit(1);
+    } else {
+        return (vectorVoid) {data, 0, n, baseTypeSize};
+    }
+}
+
+void reserveV(vectorVoid *v, size_t newCapacity) {
+    v->data = realloc(v->data, v->baseTypeSize*newCapacity);
+    if (v->data == NULL && newCapacity != 0) {
+        fprintf(stderr, "bad alloc");
+        exit(1);
+    } else {
+        v->capacity = newCapacity;
+        v->size = min2(v->size, newCapacity);
+    }
+}
+
+void clearV(vectorVoid *v) {
+    v->size = 0;
+}
+
+void shrinkToFitV(vectorVoid *v) {
+    reserveV(v, v->size);
+}
+
+void deleteVectorV(vectorVoid *v) {
+    free(v->data);
+    v->size = 0;
+    v->capacity = 0;
+}
--- libs/data_structures/voidVector/voidVector.h
+++ libs/data_structures/voidVector/voidVector.h
@@ -13,4 +13,20 @@ typedef struct {
     // если вектор хранит float - то поле baseTypeSize = sizeof(float)
 } vectorVoid;
 
+//Создаёт и возвращает пустой произвольный вектор, в котором зарезервирована память для n элементов размера baseTypeSize
+vectorVoid createVectorV(size_t n, size_t baseTypeSize);
+
+//изменяет количество памяти, отведенной для хранения произвольного вектора, так, чтобы он вмещал в себя newCapacity элементов
+void reserveV(vectorVoid *v, size_t newCapacity);
+
+//Уменьшает размер произвольного вектора по адресу v до нуля, как бы списывая все хранящиеся элементы в "мусор"
+void clearV(vectorVoid *v);
+
+//Уравнивает вместимость произвольного вектора по адресу v с количеством элементов в нем.
+void shrinkToFitV(vectorVoid *v);
+
+//Освобождает память, выделенную произвольному вектору по адресу v
+void deleteVectorV(vectorVoid *v);
+
+
 #endif
