05d710945b8f0f526cb9af4b7fa6b78d31edf455
Chupahina Sofia PV-231 <sovachupahina@yandex.ru>
2024-05-26 21:45:03 +0300

    lab 18, ex 8, realization

--- libs/data_structures/string/tasks/8_countPalindromes.c
+++ libs/data_structures/string/tasks/8_countPalindromes.c
@@ -5,13 +5,15 @@
 //возвращает количество палиндромов в строке, слова которой разделены запятыми
 int countPalindromes(char *s);
 
+//<Автоматизированные тесты>
+
 void test_countPalindromes_empty() {
     char s[] = "";
     assert(countPalindromes(s) == 0);
 }
 void test_countPalindromes_spacesOnly() {
     char s[] = "\t \n \t";
-    assert(countPalindromes(s) == 0);
+    assert(countPalindromes(s) == 1);
 }
 void test_countPalindromes_oneWordNoPalindrome() {
     char s[] = "apple";
@@ -19,7 +21,7 @@ void test_countPalindromes_oneWordNoPalindrome() {
 }
 void test_countPalindromes_oneWordPalindrome() {
     char s[] = "madam";
-    assert(countPalindromes(s) == 0);
+    assert(countPalindromes(s) == 1);
 }
 void test_countPalindromes_diverseWords() {
     char s[] = "apple,madam,cherry,ada";
@@ -31,6 +33,34 @@ void test_countPalindromes() {
     test_countPalindromes_oneWordNoPalindrome();
     test_countPalindromes_oneWordPalindrome();
     test_countPalindromes_diverseWords();
+}
+
+int countPalindromes(char *s) {
+    int counter = 0;
+    char *cur_begin = s;
+    char *cur_end = cur_begin;;
+    while (*cur_end != '\0') {
+        while (*cur_end != '\0' && *cur_end != ',') {
+            cur_end++;
+        }
+        char *right_edge = cur_end - 1;
+        char *left_edge = cur_begin;
+        bool is_palindrome = true;
+        while (left_edge < right_edge) {
+            if (*left_edge != *right_edge) {
+                is_palindrome = false;
+                break;
+            }
+            left_edge++;
+            right_edge--;
+        }
+        if (is_palindrome) {
+            counter++;
+        }
+        cur_begin = cur_end + 1;
+        cur_end = cur_begin;
+    }
+    return counter;
 }
 
 int main() {
