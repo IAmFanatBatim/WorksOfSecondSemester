0c109f8a7d382d22422da4297c390c3361b3b75c
Chupahina Sofia PV-231 <sovachupahina@yandex.ru>
2024-05-27 14:24:26 +0300

    lab 18, ex 19, realization

--- libs/data_structures/string/tasks/19_areWordLettersInString.c
+++ libs/data_structures/string/tasks/19_areWordLettersInString.c
@@ -41,6 +41,34 @@ void test_areWordLettersInString() {
     test_areWordLettersInString_someLettersIn();
 }
 
+bool areWordLettersInString(char *s, char *word) {
+    if (*s == '\0' && *word != '\0') {
+        return 0;
+    }
+    copy(s, s + strlen_(s), _stringBuffer);
+    char word_buffer[strlen_(word)];
+    char *end_of_word_buffer = copy(word, word + strlen_(word), word_buffer);
+    qsort(_stringBuffer, strlen_(s), sizeof(char), compare_chars);
+    qsort(word_buffer, strlen_(word), sizeof(char), compare_chars);
+    bool are_all_letters_in_string = true;
+    char *read_string = _stringBuffer;
+    char *read_word = word_buffer;
+    while (read_word != end_of_word_buffer) {
+        if (*read_word == '\0') {
+            break;
+        } else if (*read_string == *read_word) {
+            read_string++;
+            read_word++;
+        } else if (*read_string < *read_word) {
+            read_string++;
+        } else if (*read_string > *read_word){
+            are_all_letters_in_string = false;
+            break;
+        }
+    }
+    return are_all_letters_in_string;
+}
+
 int main() {
     test_areWordLettersInString();
     printf("All is okay");
