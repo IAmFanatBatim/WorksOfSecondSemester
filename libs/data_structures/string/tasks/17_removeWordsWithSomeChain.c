d244b3088bc83352f464bd7845e207fae8155cd9
Chupahina Sofia PV-231 <sovachupahina@yandex.ru>
2024-05-27 13:28:59 +0300

    lab 18, ex 17, realization

--- libs/data_structures/string/tasks/17_removeWordsWithSomeChain.c
+++ libs/data_structures/string/tasks/17_removeWordsWithSomeChain.c
@@ -24,7 +24,7 @@ void test_removeWordsWithSomeChain_noWords() {
 void test_removeWordsWithSomeChain_noDeletions() {
     char s[] = "cherry";
     char chain[] = "abc";
-    char s_expected[] = "";
+    char s_expected[] = "cherry";
     removeWordsWithSomeChain(s, chain);
     assertString(s_expected, s);
 }
@@ -38,7 +38,7 @@ void test_removeWordsWithSomeChain_allDeletions() {
 void test_removeWordsWithSomeChain_someDeletions() {
     char s[] = "cherry lady apple hero";
     char chain[] = "her";
-    char s_expected[] = "lady hero";
+    char s_expected[] = "lady apple";
     removeWordsWithSomeChain(s, chain);
     assertString(s_expected, s);
 }
@@ -55,11 +55,33 @@ void test_removeWordsWithSomeChain() {
     test_removeWordsWithSomeChain_noDeletions();
     test_removeWordsWithSomeChain_allDeletions();
     test_removeWordsWithSomeChain_someDeletions();
-    test_removeWordsWithSomeChain_empty();
+    test_removeWordsWithSomeChain_emptyChain();
 }
 
 void removeWordsWithSomeChain(char *s, char *chain) {
-
+    if (strlen_(chain) != 0) {
+        WordDescriptor cur_word;
+        char *read_ptr = s;
+        char *write_ptr = s;
+        while (getWord(read_ptr, &cur_word)) {
+            bool has_chain = false;
+            for (int i = 0; i <= cur_word.end - cur_word.begin - strlen_(chain); i++) {
+                if (memcmp(chain, cur_word.begin+i, strlen_(chain)) == 0) {
+                    has_chain = true;
+                    break;
+                }
+            }
+            if (!has_chain) {
+                if (write_ptr != s) {
+                    *write_ptr = ' ';
+                    write_ptr++;
+                }
+                write_ptr = copy (cur_word.begin, cur_word.end, write_ptr);
+            }
+            read_ptr = cur_word.end;
+        }
+        *write_ptr = '\0';
+    }
 }
 
 int main() {
