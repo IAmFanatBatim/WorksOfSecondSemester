f44677e917e8810291424dcd2e4befd0bf460cad
Chupahina Sofia PV-231 <sovachupahina@yandex.ru>
2024-05-27 10:32:48 +0300

    lab 18, ex 1,, realization + tests fixing

--- libs/data_structures/string/tasks/11_printWordBeforeFirstWordWithA.c
+++ libs/data_structures/string/tasks/11_printWordBeforeFirstWordWithA.c
@@ -10,6 +10,7 @@ typedef enum WordBeforeFirstWordWithAReturnCode {
 
 //Выводит слово данной строки, предшествующее первому из слов, содержащих букву "а". Регистр значения не имеет.
 WordBeforeFirstWordWithAReturnCode getWordBeforeFirstWordWithA(char *s, WordDescriptor *w);
+//<Автоматизированные тесты>
 
 void test_getWordBeforeFirstWordWithA_noWords() {
     char s[] = "\t \n \t";
@@ -27,22 +28,22 @@ void test_getWordBeforeFirstWordWithA_firstWordWithA() {
     assert(getWordBeforeFirstWordWithA(s, &word) == FIRST_WORD_WITH_A);
 }
 void test_getWordBeforeFirstWordWithA_wordFound() {
-    char s[] = "banana apple cherry";
+    char s[] = "lemon apple cherry";
     WordDescriptor word;
     assert(getWordBeforeFirstWordWithA(s, &word) == WORD_FOUND);
     char got[MAX_WORD_SIZE];
     copy(word.begin, word.end, got);
     got[word.end - word.begin] = '\0';
-    assertString("banana", got);
+    assertString("lemon", got);
 }
 void test_getWordBeforeFirstWordWithA_wordFoundACapital() {
-    char s[] = "Banana Apple Cherry";
+    char s[] = "Lemon Apple Cherry";
     WordDescriptor word;
     assert(getWordBeforeFirstWordWithA(s, &word) == WORD_FOUND);
     char got[MAX_WORD_SIZE];
     copy(word.begin, word.end, got);
     got[word.end - word.begin] = '\0';
-    assertString("Banana", got);
+    assertString("Lemon", got);
 
 }
 void test_reverseOrderWords() {
@@ -54,7 +55,44 @@ void test_reverseOrderWords() {
 }
 
 WordBeforeFirstWordWithAReturnCode getWordBeforeFirstWordWithA(char *s, WordDescriptor *w) {
+    WordDescriptor cur_word;
+    bool is_word_without_a_found = false;
+    bool is_word_with_a_found = false;
 
+    while (getWord(s, &cur_word) && !is_word_with_a_found) {
+        char *cur_char_of_word = cur_word.begin;
+        while (cur_char_of_word != cur_word.end) {
+            if (*cur_char_of_word == 'a' || *cur_char_of_word == 'A') {
+                is_word_with_a_found = true;
+                break;
+            }
+            cur_char_of_word++;
+        }
+        if (!is_word_with_a_found) {
+            is_word_without_a_found = true;
+            *w = cur_word;
+        }
+        s = cur_word.end;
+    }
+
+    if (is_word_with_a_found && is_word_without_a_found) {
+        char *cur_word_pointer = w->begin;
+        while (cur_word_pointer != w->end) {
+            printf("%c", *cur_word_pointer);
+            cur_word_pointer++;
+        }
+        printf("\n");
+        return WORD_FOUND;
+    } else if (is_word_with_a_found) {
+        printf("First word contains 'a' letter\n");
+        return FIRST_WORD_WITH_A;
+    } else if (is_word_without_a_found) {
+        printf("There are no words in 'a' letter the string\n");
+        return NOT_FOUND_A_WORD_WITH_A;
+    } else {
+        printf("There are no words in the string\n");
+        return EMPTY_STRING;
+    }
 }
 
 int main() {
