c7c288c1ca1876cf88725bad79c554bd5a5d5990
Chupahina Sofia PV-231 <sovachupahina@yandex.ru>
2024-05-27 11:10:19 +0300

    lab 18, ex 13, tests

---
+++ libs/data_structures/string/tasks/13_areIdenticalWordsInString.c
@@ -0,0 +1,41 @@
+#include <ctype.h>
+#include "string_support_functions.c"
+
+//Определяет, есть ли в данной строке одинаковые слова
+bool areIdenticalWordsInString(char *s);
+
+//<Автоматизированные тесты>
+
+void test_areIdenticalWordsInString_noWords() {
+    char s[] = "";
+    assert(areIdenticalWordsInString(s) == 0);
+}
+void test_areIdenticalWordsInString_oneWord() {
+    char s[] = "apple";
+    assert(areIdenticalWordsInString(s) == 0);
+}
+void test_areIdenticalWordsInString_noIdenticalWords() {
+    char s[] = "apple lemon banana";
+    assert(areIdenticalWordsInString(s) == 0);
+}
+void test_areIdenticalWordsInString_hasIdenticalWords() {
+    char s[] = "apple lemon apple banana";
+    assert(areIdenticalWordsInString(s) == 1);
+}
+void test_areIdenticalWordsInString_hasMoreIdenticalWords() {
+    char s[] = "apple lemon apple apple";
+    assert(areIdenticalWordsInString(s) == 1);
+
+}
+void test_areIdenticalWordsInString() {
+    test_areIdenticalWordsInString_noWords();
+    test_areIdenticalWordsInString_oneWord();
+    test_areIdenticalWordsInString_noIdenticalWords();
+    test_areIdenticalWordsInString_hasIdenticalWords();
+    test_areIdenticalWordsInString_hasMoreIdenticalWords();
+}
+
+int main() {
+    test_areIdenticalWordsInString();
+}
+
