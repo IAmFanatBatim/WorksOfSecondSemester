5f6e2a7c958dba8172e7b905b253309d21f9e41e
Chupahina Sofia PV-231 <sovachupahina@yandex.ru>
2024-05-27 13:40:47 +0300

    lab 18, ex 18, tests

---
+++ libs/data_structures/string/tasks/18_fillStringWithLessWordsFromAnother.c
@@ -0,0 +1,66 @@
+#include <ctype.h>
+#include "string_support_functions.c"
+
+//Дополняет ту из строк. в которой слов меньше, последними словами из другой строки
+void fillStringWithLessWordsFromAnother(char *s1, char *s2);
+
+//<Автоматизированные тесты>
+
+void test_fillStringWithLessWordsFromAnother_noWords() {
+    char s1[MAX_STRING_SIZE] = "";
+    char s2[MAX_STRING_SIZE] = "\t \n \t";
+    char s_expected1[] = "";
+    char s_expected2[] = "\t \n \t";
+    fillStringWithLessWordsFromAnother(s1, s2);
+    assertString(s_expected1, s1);
+    assertString(s_expected2, s2);
+}
+void test_fillStringWithLessWordsFromAnother_sameWordsAmount() {
+    char s1[MAX_STRING_SIZE] = "cherry apple";
+    char s2[MAX_STRING_SIZE] = "lemon banana";
+    char s_expected1[] = "cherry apple";
+    char s_expected2[] = "lemon banana";
+    fillStringWithLessWordsFromAnother(s1, s2);
+    assertString(s_expected1, s1);
+    assertString(s_expected2, s2);
+}
+void test_fillStringWithLessWordsFromAnother_firstLonger() {
+    char s1[MAX_STRING_SIZE] = "cherry apple banana";
+    char s2[MAX_STRING_SIZE] = "lemon";
+    char s_expected1[] = "cherry apple banana";
+    char s_expected2[] = "lemon apple banana";
+    fillStringWithLessWordsFromAnother(s1, s2);
+    assertString(s_expected1, s1);
+    assertString(s_expected2, s2);
+}
+void test_fillStringWithLessWordsFromAnother_firstShorter() {
+    char s1[MAX_STRING_SIZE] = "cherry";
+    char s2[MAX_STRING_SIZE] = "lemon apple banana";
+    char s_expected1[] = "cherry apple banana";
+    char s_expected2[] = "lemon apple banana";
+    fillStringWithLessWordsFromAnother(s1, s2);
+    assertString(s_expected1, s1);
+    assertString(s_expected2, s2);
+}
+void test_fillStringWithLessWordsFromAnother_oneEmpty() {
+    char s1[MAX_STRING_SIZE] = "cherry";
+    char s2[MAX_STRING_SIZE] = "";
+    char s_expected1[] = "cherry";
+    char s_expected2[] = "cherry";
+    fillStringWithLessWordsFromAnother(s1, s2);
+    assertString(s_expected1, s1);
+    assertString(s_expected2, s2);
+
+}
+void test_fillStringWithLessWordsFromAnother() {
+    test_fillStringWithLessWordsFromAnother_noWords();
+    test_fillStringWithLessWordsFromAnother_sameWordsAmount();
+    test_fillStringWithLessWordsFromAnother_firstLonger();
+    test_fillStringWithLessWordsFromAnother_firstShorter();
+    test_fillStringWithLessWordsFromAnother_oneEmpty();
+}
+
+int main() {
+    test_fillStringWithLessWordsFromAnother();
+}
+
