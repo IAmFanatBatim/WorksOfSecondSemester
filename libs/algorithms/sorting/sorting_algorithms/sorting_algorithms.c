f42373954af813da94781ddd7ceba1e7b13311d2
Chupahina Sofia PV-231 <sovachupahina@yandex.ru>
2024-06-07 15:44:13 +0300

    lab 19, ex 4, InsertiionSort function adding

--- libs/algorithms/sorting/sorting_algorithms/sorting_algorithms.c
+++ libs/algorithms/sorting/sorting_algorithms/sorting_algorithms.c
@@ -3,7 +3,7 @@
 
 void bubbleSort(int *array, size_t size) {
     bool was_exchange_required = true;
-    for (int i = 0; i < size && was_exchange_required; i++) {
+    for (size_t i = 0; i < size && was_exchange_required; i++) {
         was_exchange_required = false;
         for (size_t j = 0; j < size - i - 1; j++) {
             if (array[j] > array[j+1]) {
@@ -23,5 +23,17 @@ void selectionSort(int *array, size_t size) {
             }
         }
         swapVoid(&array[i], &array[min_pos], sizeof(int));
+    }
+}
+
+void insertionSort(int *array, size_t size) {
+    for (int i = 0; i < size; i++) {
+        int cur_el = array[i];
+        size_t cur_ind = i;
+        while (cur_ind > 0 && array[cur_ind-1] > cur_el) {
+            array[cur_ind] = array[cur_ind-1];
+            cur_ind--;
+        }
+        array[cur_ind] = cur_el;
     }
 }
